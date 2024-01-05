9819ec2e4c8a4a5ff047500e60e7f3c0b8915ecc
Dylan <34799786+FrickinFrickFlynn@users.noreply.github.com>
2024-01-04 22:10:21 -0500

    Update assembler.js
    testing something

--- simulator/assembler.js
+++ simulator/assembler.js
@@ -240,7 +240,7 @@ function SimulatorWidget(node) {
   }
 
   function Memory() {
-    var memArray = new Array(0x600);
+    var memArray = new Array(0x800);
 
     function set(addr, val) {
       return memArray[addr] = val;
@@ -303,7 +303,7 @@ function SimulatorWidget(node) {
     var regX = 0;
     var regY = 0;
     var regP = 0;
-    var regPC = 0x600;
+    var regPC = 0x800;
     var regSP = 0xff;
     var codeRunning = false;
     var debug = false;
@@ -1684,11 +1684,11 @@ function SimulatorWidget(node) {
     // reset() - Reset CPU and memory.
     function reset() {
       display.reset();
-      for (var i = 0; i < 0x600; i++) { // clear ZP, stack and screen
+      for (var i = 0; i < 0x800; i++) { // clear ZP, stack and screen
         memory.set(i, 0x00);
       }
       regA = regX = regY = 0;
-      regPC = 0x600;
+      regPC = 0x800;
       regSP = 0xff;
       regP = 0x30;
       updateDebugInfo();
@@ -1891,7 +1891,7 @@ function SimulatorWidget(node) {
     
     // Assembles the code into memory
     function assembleCode() {
-      var BOOTSTRAP_ADDRESS = 0x600;
+      var BOOTSTRAP_ADDRESS = 0x800;
 
       wasOutOfRangeBranch = false;
   
@@ -2465,7 +2465,7 @@ function SimulatorWidget(node) {
 
     // Dump binary as hex to new window
     function hexdump() {
-      openPopup(memory.format(0x600, codeLen), 'Hexdump');
+      openPopup(memory.format(0x800, codeLen), 'Hexdump');
     }
 
     // TODO: Create separate disassembler object?
@@ -2598,7 +2598,7 @@ function SimulatorWidget(node) {
     }
 
     function disassemble() {
-      var startAddress = 0x600;
+      var startAddress = 0x800;
       var currentAddress = startAddress;
       var endAddress = startAddress + codeLen;
       var instructions = [];
